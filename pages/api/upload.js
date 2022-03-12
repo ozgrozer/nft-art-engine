bbf850be52c910711272ae788fd67d74b145a6dc
Ozgur <ozgr@live.com>
2022-03-13 01:32:21 +0300

    Upload folder

---
+++ pages/api/upload.js
@@ -0,0 +1,49 @@
+import fs from 'fs'
+import multer from 'multer'
+import nextConnect from 'next-connect'
+
+const upload = multer({
+  storage: multer.diskStorage({
+    filename: (req, file, cb) => cb(null, file.originalname),
+    destination: (req, file, cb) => {
+      if (file.originalname !== '.DS_Store') {
+        const splitFieldname = file.fieldname.split('/')
+        const folderPath = `./public/uploads/${splitFieldname[1]}`
+
+        if (!fs.existsSync(folderPath)) fs.mkdirSync(folderPath)
+
+        cb(null, folderPath)
+      } else {
+        cb(null, './public/uploads')
+      }
+    },
+    fileFilter: (req, file, cb) => {
+      if (file.originalname === '.DS_Store') {
+        cb(null, false)
+      }
+    }
+  })
+})
+
+const apiRoute = nextConnect({
+  onError (error, req, res) {
+    res.status(501).json({ error: `Sorry something Happened! ${error.message}` })
+  },
+  onNoMatch (req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` })
+  }
+})
+
+apiRoute.use(upload.any())
+
+apiRoute.post((req, res) => {
+  res.status(200).json({ success: true })
+})
+
+export default apiRoute
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,52 @@
+import axios from 'axios'
 import Head from 'next/head'
+import { useRef } from 'react'
 
 export default () => {
+  const folderInputRef = useRef(null)
+  const chooseFolder = () => folderInputRef.current.click()
+
+  const folderInputOnChange = async event => {
+    const formData = new window.FormData()
+    for (const key in event.target.files) {
+      const item = event.target.files[key]
+      formData.append(item.webkitRelativePath, item)
+    }
+
+    const response = await axios({
+      method: 'post',
+      data: formData,
+      url: '/api/upload',
+      headers: { 'content-type': 'multipart/form-data' },
+      onUploadProgress: event => {
+        console.log('progress:', Math.round((event.loaded * 100) / event.total))
+      }
+    })
+    console.log('response:', response.data)
+  }
+
   return (
     <>
       <Head>
         <title>NFT Art Engine</title>
       </Head>
 
-      <div>Hello World!</div>
+      <div>
+        <button type='button' onClick={chooseFolder}>
+          Choose folder
+        </button>
+
+        <input
+          type='file'
+          name='files'
+          directory='true'
+          mozdirectory='true'
+          ref={folderInputRef}
+          webkitdirectory='true'
+          style={{ display: 'none' }}
+          onChange={folderInputOnChange}
+        />
+      </div>
     </>
   )
 }
