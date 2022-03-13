eb3a63c0bd090787caae5ed5b19516d6367a03f9
Ozgur <ozgr@live.com>
2022-03-13 04:25:24 +0300

    Use custom layer configurations

--- engine/index.js
+++ engine/index.js
@@ -3,4 +3,6 @@ const path = require('path')
 const { startCreating, buildSetup } = require(path.join(__dirname, 'src', 'main.js'))
 
 buildSetup()
-startCreating()
+startCreating({
+  layerConfigurations: JSON.parse(process.env.layerConfigurations)
+})
