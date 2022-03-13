70302ee15e61d69b7a9290a3a2e2aebcd364e1ac
Ozgur <ozgr@live.com>
2022-03-13 22:05:56 +0300

    Add my own design system

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,8 @@ import axios from 'axios'
 import Head from 'next/head'
 import { useRef, useState } from 'react'
 
-import styles from '@components/styles/home.module.scss'
+import clx from '@components/functions/clx'
+import styles from '@components/styles/pages/home.module.scss'
 
 const ChooseFolder = ({ setUploadedFiles }) => {
   const folderInputRef = useRef(null)
@@ -34,7 +35,7 @@ const ChooseFolder = ({ setUploadedFiles }) => {
       <button
         type='button'
         onClick={chooseFolder}
-        className='text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-800'
+        className={clx(styles.button, styles.block)}
       >
         Choose Folder
       </button>
@@ -80,28 +81,26 @@ const AdjustLayers = ({ uploadedFiles, setGeneratedImages }) => {
   }
 
   return (
-    <form className='container mx-auto'>
-      <fieldset disabled>
-        <label htmlFor='layers' className='block text-sm font-medium text-gray-700'>
-          Layers
-        </label>
+    <form className={styles.form}>
+      <fieldset disabled={false}>
+        <div>
+          <label htmlFor='layers' className={styles.label}>
+            Layers
+          </label>
+        </div>
 
         <textarea
           rows={7}
           id='layers'
           name='layers'
           value={layers}
+          className={styles.textarea}
           onChange={e => setLayers(e.target.value)}
-          className='mt-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md'
         />
-
-        <p className='mt-2 text-sm text-gray-500'>
-          Move layers from back to front
-        </p>
 
         <button
           onClick={generateImages}
-          className='mt-5 text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+          className={clx(styles.button, styles.block, styles.green)}
         >
           Generate Images
         </button>
@@ -111,7 +110,7 @@ const AdjustLayers = ({ uploadedFiles, setGeneratedImages }) => {
 }
 
 export default () => {
-  const [uploadedFiles, setUploadedFiles] = useState([{ destination: './././one' }])
+  const [uploadedFiles, setUploadedFiles] = useState()
   const [generatedImages, setGeneratedImages] = useState()
 
   return (
